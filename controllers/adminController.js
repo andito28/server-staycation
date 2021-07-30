2e5abef47a0c2d8bbba37e4c7bab4300336b4acf
andito28 <andito763@gmail.com>
2021-07-30 21:12:30 +0800

    fungsi update data bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const Category = require('../models/Category');
 const Bank = require('../models/Bank');
+const fs = require('fs-extra');
+const path = require('path');
 
 module.exports = {
     viewDashboard : (req,res) => {
@@ -83,7 +85,12 @@ module.exports = {
     addBank : async(req,res) => {
         try{
             const {nameBank,nomorRekening,name} = req.body;
-            await Bank.create({nameBank:nameBank,name:name,nomorRekening:nomorRekening,imageUrl : req.file.filename}); 
+            await Bank.create({
+                nameBank,
+                name,
+                nomorRekening,
+                imageUrl : `images/${req.file.filename}`
+            }); 
             req.flash('alertMessage','Success Add Bank');
             req.flash('alertStatus','success');
             res.redirect('/admin/bank');
@@ -92,6 +99,38 @@ module.exports = {
             req.flash('alertStatus','danger')
             res.redirect('/admin/bank');
         }
+    },
+
+    editBank : async(req,res) => {
+        try{
+            const {nameBank,nomorRekening,name,id} = req.body;
+            
+            const bank = await Bank.findOne({_id : id});
+
+            if(req.file == undefined){
+               bank.nameBank = nameBank;
+               bank.nomorRekening = nomorRekening;
+               bank.name = name;
+               bank.save();
+               req.flash('alertMessage','Success Update Bank');
+               req.flash('alertStatus','success');
+               res.redirect('/admin/bank');
+            }else{
+               await fs.unlink(path.join(`public/${bank.imageUrl}`));
+               bank.nameBank = nameBank;
+               bank.nomorRekening = nomorRekening;
+               bank.name = name;
+               bank.imageUrl = `images/${req.file.filename}`;
+               await bank.save();
+               req.flash('alertMessage','Success Update Bank');
+               req.flash('alertStatus','success');
+               res.redirect('/admin/bank');
+            }
+            }catch(error){
+                req.flash('alertMessage',`${error.message}`);
+                req.flash('alertStatus','danger')
+                res.redirect('/admin/bank');
+            }
     },
 
     viewItem : (req,res) => {
